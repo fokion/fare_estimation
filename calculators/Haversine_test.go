9287ae05135f1f642bf4a22ae038f5e16a95289d
Fokion Sotiropoulos <fokion.s@gmail.com>
2022-02-25 20:02:39 +0000

    start working on tests for haversine calculation
     used an assert library in order to make my life easier with the Equality checks
    - see https://pkg.go.dev/github.com/stretchr/testify/assert
    
    - test fails as expected as nothing is written for calculation

--- calculators/Haversine_test.go
+++ calculators/Haversine_test.go
@@ -1,8 +1,21 @@
 package calculators
 
-import "testing"
+import (
+	"fare_estimation/models"
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
 
 func TestCalculate_in_a_straight_line(t *testing.T) {
 	//given two points 55.94429 -3.20623 and 55.93985  -3.22046 it is a ~0.9656 km line
-	assert.Equal(t, 0.9656)
+	haversine := Haversine{
+		From: &models.Point{Latitude: 55.94429, Longitude: -3.20623},
+		To:   &models.Point{Latitude: 55.93985, Longitude: -3.22046},
+	}
+	distance, err := haversine.GetDistance()
+	if err != nil {
+		assert.Equal(t, 0.9656, distance)
+	}
+	t.Fail()
+
 }
