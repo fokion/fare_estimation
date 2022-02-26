9db0994238eef6116740537fc1159708c3bbdf63
Fokion Sotiropoulos <fokion.s@gmail.com>
2022-02-26 12:56:54 +0000

    speed calculator

--- calculators/Speed.go
+++ calculators/Speed.go
@@ -1,9 +1,40 @@
 package calculators
 
-import "fare_estimation/errors"
+import (
+	"fare_estimation/errors"
+	"fmt"
+)
 
-//GetSpeedInKm calculate the speed based on the distance covered.
-func GetSpeedInKm(distanceInKm float64, startTimestamp int64, endTimestamp int64) (float64, error) {
+const (
+	MAX_SPEED_IN_KM = 100
+)
+
+type SpeedCalculator interface {
+	GetSpeed(distance float64, startTimestamp int64, endTimestamp int64) (float64, error)
+	GetMaxSpeed() float64
+	PrintMaxSpeed() string
+}
+
+type SpeedInKMCalculator struct {
+	Max float64
+}
+
+func NewSpeedCalculatorInKM() *SpeedInKMCalculator {
+	return &SpeedInKMCalculator{
+		MAX_SPEED_IN_KM,
+	}
+}
+
+func (c *SpeedInKMCalculator) GetMaxSpeed() float64 {
+	return c.Max
+}
+
+func (c *SpeedInKMCalculator) PrintMaxSpeed() string {
+	return fmt.Sprintf("%fkm/h", c.Max)
+}
+
+//GetSpeed calculate the speed based on the distance covered.
+func (c *SpeedInKMCalculator) GetSpeed(distanceInKm float64, startTimestamp int64, endTimestamp int64) (float64, error) {
 	deltaTime := (endTimestamp - startTimestamp) / 3600
 	if deltaTime > 0 {
 		return distanceInKm / float64(deltaTime), nil
--- calculators/Speed_test.go
+++ calculators/Speed_test.go
@@ -9,19 +9,22 @@ import (
 func TestGetSpeedInKm(t *testing.T) {
 	timeNow := time.Now()
 	timeReached := timeNow.Add(time.Hour * 1)
-	expected, _ := GetSpeedInKm(100, timeNow.Unix(), timeReached.Unix())
+	calc := SpeedInKMCalculator{}
+	expected, _ := calc.GetSpeed(100, timeNow.Unix(), timeReached.Unix())
 	assert.Equal(t, float64(100), expected)
 }
 
 func TestGetSpeedInKm_test_20(t *testing.T) {
 	timeNow := time.Now()
 	timeReached := timeNow.Add(time.Hour * 1)
-	expected, _ := GetSpeedInKm(20, timeNow.Unix(), timeReached.Unix())
+	calc := SpeedInKMCalculator{}
+	expected, _ := calc.GetSpeed(20, timeNow.Unix(), timeReached.Unix())
 	assert.Equal(t, float64(20), expected)
 }
 
 func TestGetSpeedInKm_test_error(t *testing.T) {
 	timeNow := time.Now()
-	_, err := GetSpeedInKm(20, timeNow.Unix(), timeNow.Unix())
+	calc := SpeedInKMCalculator{}
+	_, err := calc.GetSpeed(20, timeNow.Unix(), timeNow.Unix())
 	assert.Error(t, err)
 }
