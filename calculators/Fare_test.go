8f52b26c555a6056317a9ecc92b59fe27a7af109
Fokion Sotiropoulos <fokion.s@gmail.com>
2022-02-27 19:38:23 +0000

    improve coverage

--- calculators/Fare_test.go
+++ calculators/Fare_test.go
@@ -88,3 +88,36 @@ func TestCleanup(t *testing.T) {
 	assert.Equal(t, 2, len(cleanPoints))
 	assert.Equal(t, date.Add(time.Minute*10).Unix(), cleanPoints[len(cleanPoints)-1].Timestamp)
 }
+
+func TestCalculateFare_when_we_are_changing_rates(t *testing.T) {
+
+	distanceCalculator := NewHarversineCalculatorInKM()
+	speedCalculator := NewSpeedCalculatorInKM()
+	farecalc := NewFareCalculator(distanceCalculator, speedCalculator, GetDefaultRates(), 10, models.FLAG_RATE, models.MINIMUM_RATE)
+	date := time.Date(2022, 02, 25, 23, 54, 0, 0, time.UTC)
+
+	pointA := &models.Point{Latitude: 55.94429, Longitude: -3.20623, Timestamp: date.Unix()}
+	pointB := &models.Point{Latitude: 55.93985, Longitude: -3.22046, Timestamp: date.Add(time.Minute * 5).Unix()}
+	pointC := &models.Point{Latitude: 55.94429, Longitude: -3.20623, Timestamp: date.Add(time.Minute * 10).Unix()}
+
+	_, err := farecalc.CalculateFare(pointA, pointB)
+	if err != nil {
+		t.Fail()
+	}
+	//this is after the switch so there will be a portion with the old fare and some with the new one
+	fareReturn, err := farecalc.CalculateFare(pointB, pointC)
+
+	//speed in point B to C
+	distanceCovered, _ := distanceCalculator.GetDistance(pointB, pointC)
+	speed, _ := speedCalculator.GetSpeed(distanceCovered, pointB.Timestamp, pointC.Timestamp)
+
+	assert.True(t, speed < speedCalculator.GetMaxSpeed())
+
+	//the difference between time in point B and going to the next rate is 1 minute
+
+	distanceWithinDayRate := float64(speed) / 60.0
+	expected := distanceWithinDayRate*DAY_RATE + (distanceCovered-distanceWithinDayRate)*NIGHT_RATE
+
+	assert.Equal(t, new(big.Float).SetPrec(4).SetFloat64(expected), new(big.Float).SetPrec(4).SetFloat64(fareReturn))
+
+}
